5c419f383be2109e68443b78017fe7d0761d6d80
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2022-09-25 16:24:34 +0100

    Add date library

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime } = require('luxon');
+
 module.exports = function (eleventyConfig) {
   // Copy the `css` directory to the output
   eleventyConfig.addPassthroughCopy('./src/css/');
@@ -6,6 +8,18 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
   eleventyConfig.addPassthroughCopy("./src/js/");
+
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'dd LLL yyyy'
+    );
+  });
+
+  eleventyConfig.addFilter('shortDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'dd LLL'
+    );
+  });
 
   return {
     dir: {
