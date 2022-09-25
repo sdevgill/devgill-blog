774a4338a400dcd143837fe78ce5220a114a392e
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2022-09-25 14:27:56 +0100

    Add src and public and about page

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
 module.exports = function (eleventyConfig) {
   // Copy the `css` directory to the output
-  eleventyConfig.addPassthroughCopy('css');
-
+  eleventyConfig.addPassthroughCopy('./src/css/');
   // Watch the `css` directory for changes
-  eleventyConfig.addWatchTarget('css');
+  eleventyConfig.addWatchTarget('./src/css/');
+  eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
 };
