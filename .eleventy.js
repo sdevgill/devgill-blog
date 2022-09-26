ef405f9d392cab7011dce46253bd44d5d2bc5b5f
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2022-09-26 11:30:33 +0100

    Add readingTime

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require('luxon');
+const readingTime = require('reading-time');
 
 module.exports = function (eleventyConfig) {
   // Copy the `css` directory to the output
@@ -8,17 +9,20 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
   eleventyConfig.addPassthroughCopy("./src/js/");
-
+  // Date formatting (human readable)
   eleventyConfig.addFilter('readableDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
       'dd LLL yyyy'
     );
   });
-
   eleventyConfig.addFilter('shortDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
       'dd LLL'
     );
+  });
+  // Reading time
+  eleventyConfig.addFilter('readingTime', (text) => {
+    return readingTime(text).text;
   });
 
   return {
